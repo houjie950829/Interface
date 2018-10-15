b66c914eaa244dda4b54d53ee661a0e393c9cc86
于志强 <yuzhiqiang@gouuse.cn>
2018-10-15 10:52:22 +0800

    南海企业微信

---
+++ models/results.py
@@ -0,0 +1,10 @@
+# coding=utf-8
+
+
+def results(expected, actual, parameter, Results):
+    return {
+        'expected': expected,
+        'actual': actual,
+        'parameter': parameter,
+        'Results': Results,
+    }
---
+++ mysite/polls/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('get_report/', views.get_report),
+    path('report/', views.report),
+]
